ecd997c39eb1f9f9c7292d4bb796ee10dca480cb
Nick Hosman <nickhosman@gmail.com>
2023-08-03 14:08:12 -0700

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -7,6 +7,7 @@ const { User } = require("../../db/models");
 
 const router = express.Router();
 
+// Log in
 router.post("/", async (req, res, next) => {
     const { credential, password } = req.body;
 
@@ -41,6 +42,12 @@ router.post("/", async (req, res, next) => {
     return res.json({
         user: safeUser,
     });
+});
+
+// Log out
+router.delete("/", (_req, res) => {
+    res.clearCookie("token");
+    return res.json({ message: "success" });
 });
 
 module.exports = router;
