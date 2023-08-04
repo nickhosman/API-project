e6ca1ef18da43b6eba67570edb1f3f39e9b5b841
Nick Hosman <nickhosman@gmail.com>
2023-08-04 13:30:31 -0700

    Add options.tableName on seeder

--- backend/db/seeders/20230804174457-demo-user.js
+++ backend/db/seeders/20230804174457-demo-user.js
@@ -8,6 +8,8 @@ if (process.env.NODE_ENV === "production") {
     options.schema = process.env.SCHEMA;
 }
 
+options.tableName = "Users";
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
