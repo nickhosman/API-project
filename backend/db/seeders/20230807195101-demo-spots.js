ef03bb72a25538a2c240dcf0e8f3eeaf931f40d4
Nick Hosman <nickhosman@gmail.com>
2023-08-07 16:58:11 -0700

    debugging with seeders for live

--- backend/db/seeders/20230807195101-demo-spots.js
+++ backend/db/seeders/20230807195101-demo-spots.js
@@ -10,15 +10,6 @@ if (process.env.NODE_ENV === "production") {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        /**
-         * Add seed commands here.
-         *
-         * Example:
-         * await queryInterface.bulkInsert('People', [{
-         *   name: 'John Doe',
-         *   isBetaMember: false
-         * }], {});
-         */
         await Spot.bulkCreate(
             [
                 {
