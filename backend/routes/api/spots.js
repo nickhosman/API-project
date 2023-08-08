1b61c49348a3cea765d4506d2149a9793f8988f1
Nick Hosman <nickhosman@gmail.com>
2023-08-08 13:14:46 -0700

    Add end point for creating a new spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const sequelize = require("sequelize");
+const {
+    setTokenCookie,
+    requireAuth,
+    restoreUser,
+} = require("../../utils/auth");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 
 const { Spot, SpotImage, User, Review } = require("../../db/models");
 
@@ -35,9 +41,50 @@ router.get("/", async (req, res) => {
     return res.json(spots);
 });
 
-router.get("/:spotId", async (req, res) => {
+router.get("/current", requireAuth, async (req, res, next) => {
+    const spots = await Spot.findAll({ where: { ownerId: req.user.id } });
+
+    let Spots = [];
+
+    for (const spot of spots) {
+        // Add average rating to object
+        const reviews = await Review.findAll({ where: { spotId: spot.id } });
+
+        const reviewCount = reviews.length;
+
+        const starTotal = reviews.reduce((total, review) => {
+            total += review.stars;
+            return total;
+        }, 0);
+
+        const avgRating = starTotal / reviewCount;
+        spot.dataValues.avgRating = avgRating;
+
+        // Add previewImage url to object
+        const previewImage = await SpotImage.findOne({
+            where: { spotId: spot.id, preview: true },
+        });
+
+        if (previewImage) {
+            spot.dataValues.previewImage = previewImage.url;
+        }
+
+        Spots.push(spot.dataValues);
+    }
+
+    return res.json({ Spots });
+});
+
+router.get("/:spotId", async (req, res, next) => {
     const spot = await Spot.findByPk(req.params.spotId);
 
+    if (!spot) {
+        const err = new Error("Spot couldn't be found");
+        err.title = "Spot couldn't be found";
+        err.errors = { message: "Spot couldn't be found" };
+        err.status = 404;
+        return next(err);
+    }
     // Get reviews for this spot
     const reviews = await Review.findAll({
         where: { spotId: req.params.spotId },
@@ -71,6 +118,72 @@ router.get("/:spotId", async (req, res) => {
         attributes: ["id", "firstName", "lastName"],
     });
     spot.dataValues.Owner = owner;
+
+    return res.json(spot);
+});
+
+const validateNewSpot = [
+    check("address")
+        .exists({ checkFalsy: true })
+        .withMessage("Street address is required"),
+    check("city").exists({ checkFalsy: true }).withMessage("City is required"),
+    check("state")
+        .exists({ checkFalsy: true })
+        .withMessage("State is required"),
+    check("country")
+        .exists({ checkFalsy: true })
+        .withMessage("Country is required"),
+    check("lat")
+        .exists({ checkFalsy: true })
+        .custom((value) => {
+            return parseInt(value) < 90 && parseInt(value) > -90;
+        })
+        .withMessage("Latitude is not valid"),
+    check("lng")
+        .exists({ checkFalsy: true })
+        .custom((value) => {
+            return parseInt(value) < 180 && parseInt(value) > -180;
+        })
+        .withMessage("Longitude is not valid"),
+    check("name")
+        .exists({ checkFalsy: true })
+        .isLength({ max: 50 })
+        .withMessage("Name must be less than 50 characters"),
+    check("description")
+        .exists({ checkFalsy: true })
+        .withMessage("Description is required"),
+    check("price")
+        .exists({ checkFalsy: true })
+        .withMessage("Price per day is required"),
+    handleValidationErrors,
+    requireAuth,
+];
+
+router.post("/", validateNewSpot, async (req, res, next) => {
+    const {
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price,
+    } = req.body;
+
+    const spot = await Spot.create({
+        ownerId: req.user.id,
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price,
+    });
 
     return res.json(spot);
 });
