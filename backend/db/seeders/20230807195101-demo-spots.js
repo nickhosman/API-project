dc5fe93ca2bdc1d30891887bbc54e4a8e91061e8
Nick Hosman <nickhosman@gmail.com>
2023-08-07 18:42:20 -0700

    even more seeder debugging

--- backend/db/seeders/20230807195101-demo-spots.js
+++ backend/db/seeders/20230807195101-demo-spots.js
@@ -10,6 +10,7 @@ if (process.env.NODE_ENV === "production") {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
+        options.tableName = "Spots";
         await Spot.bulkCreate(
             [
                 {
