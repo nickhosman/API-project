73caf3cdda161b0a74d973bc6c6d9d3e0251ca83
Nick Hosman <nickhosman@gmail.com>
2023-08-08 10:51:07 -0700

    Add endpoint for getting a specific spot by id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const sequelize = require("sequelize");
 
-const { Spot } = require("../../db/models");
-const { SpotImage } = require("../../db/models");
-const { Review } = require("../../db/models");
+const { Spot, SpotImage, User, Review } = require("../../db/models");
 
 const router = express.Router();
 
@@ -14,14 +12,14 @@ router.get("/", async (req, res) => {
         // Add average rating to object
         const reviews = await Review.findAll({ where: { spotId: spot.id } });
 
-        const starCount = reviews.length;
+        const reviewCount = reviews.length;
 
         const starTotal = reviews.reduce((total, review) => {
             total += review.stars;
             return total;
         }, 0);
 
-        const avgRating = starTotal / starCount;
+        const avgRating = starTotal / reviewCount;
         spot.dataValues.avgRating = avgRating;
 
         // Add previewImage url to object
@@ -35,6 +33,46 @@ router.get("/", async (req, res) => {
     }
 
     return res.json(spots);
+});
+
+router.get("/:spotId", async (req, res) => {
+    const spot = await Spot.findByPk(req.params.spotId);
+
+    // Get reviews for this spot
+    const reviews = await Review.findAll({
+        where: { spotId: req.params.spotId },
+    });
+
+    const numReviews = reviews.length;
+    // Find the total number of stars across reviews for this spot
+    const starTotal = reviews.reduce((total, review) => {
+        total += review.stars;
+        return total;
+    }, 0);
+
+    // Get the average rating
+    const avgStarRating = starTotal / numReviews;
+    spot.dataValues.numReviews = numReviews;
+    spot.dataValues.avgStarRating = avgStarRating;
+
+    // Get the images for this spot
+    const spotImages = await SpotImage.findAll({
+        where: { spotId: req.params.spotId },
+        attributes: ["id", "url", "preview"],
+    });
+    // console.log(spotImages);
+    spot.dataValues.SpotImages = [];
+    for (const spotImage of spotImages) {
+        spot.dataValues.SpotImages.push(spotImage.dataValues);
+    }
+
+    // Get the owner of this spot by Id
+    const owner = await User.findByPk(spot.ownerId, {
+        attributes: ["id", "firstName", "lastName"],
+    });
+    spot.dataValues.Owner = owner;
+
+    return res.json(spot);
 });
 
 module.exports = router;
