e981dc71f5972b6d08bf4c7487ac975afcb807f8
Nick Hosman <nickhosman@gmail.com>
2023-08-09 10:45:16 -0700

    Add endpoint to add an image to a review by id

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -48,6 +48,50 @@ router.get("/current", requireAuth, async (req, res, next) => {
     res.json({ Reviews });
 });
 
-// Get all reviews for a spot by id
+// Add an image to a review based on review id
+router.post("/:reviewId/images", requireAuth, async (req, res, next) => {
+    const review = await Review.findByPk(req.params.reviewId);
+    const reviewImages = await ReviewImage.findAll({
+        where: { reviewId: req.params.reviewId },
+    });
+
+    if (reviewImages.length >= 10) {
+        console.log("reviewImages length:", reviewImages.length);
+        const err = new Error(
+            "Maximum number of images for this resource was reached"
+        );
+        err.title = "Maximum number of images for this resource was reached";
+        err.errors = {
+            message: "Maximum number of images for this resource was reached",
+        };
+        err.status = 403;
+        return next(err);
+    }
+
+    if (review) {
+        if (review.userId === req.user.id) {
+            const { url } = req.body;
+
+            const newImage = await ReviewImage.create({
+                reviewId: parseInt(req.params.reviewId),
+                url,
+            });
+
+            const data = newImage.toJSON();
+
+            delete data.reviewId;
+            delete data.updatedAt;
+            delete data.createdAt;
+
+            return res.json(data);
+        }
+    }
+
+    const err = new Error("Review couldn't be found");
+    err.title = "Review couldn't be found";
+    err.errors = { message: "Review couldn't be found" };
+    err.status = 404;
+    return next(err);
+});
 
 module.exports = router;
