953640cffc36c3501a8b6c73fc2200223b3e91e4
Nick Hosman <nickhosman@gmail.com>
2023-08-10 09:35:46 -0700

    Add endpoints for deleting spot and review images

---
+++ backend/routes/api/review-images.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { requireAuth } = require("../../utils/auth");
+
+const { Review, Spot, User, ReviewImage } = require("../../db/models");
+
+const router = express.Router();
+
+// Delete a review image
+router.delete("/:imageId", requireAuth, async (req, res, next) => {
+    const image = await ReviewImage.unscoped().findByPk(req.params.imageId);
+    if (image) {
+        const review = await Review.findByPk(image.reviewId);
+        if (req.user.id === review.userId) {
+            image.destroy();
+            return res.json({ message: "Succesfully deleted" });
+        }
+    }
+
+    const err = new Error("Spot couldn't be found");
+    err.title = "Spot couldn't be found";
+    err.errors = { message: "Spot couldn't be found" };
+    err.status = 404;
+    return next(err);
+});
+
+module.exports = router;
---
+++ backend/routes/api/spot-images.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { requireAuth } = require("../../utils/auth");
+
+const { Spot, SpotImage, User } = require("../../db/models");
+
+const router = express.Router();
+
+// Delete a spot image
+router.delete("/:imageId", requireAuth, async (req, res, next) => {
+    const image = await SpotImage.findByPk(req.params.imageId);
+    const spot = await Spot.findByPk(image.spotId);
+
+    if (image) {
+        if (req.user.id === spot.ownerId) {
+            image.destroy();
+            return res.json({ message: "Succesfully deleted" });
+        }
+    }
+
+    const err = new Error("Spot couldn't be found");
+    err.title = "Spot couldn't be found";
+    err.errors = { message: "Spot couldn't be found" };
+    err.status = 404;
+    return next(err);
+});
+
+module.exports = router;
