b521b28d148cf5d4808c68d245e96730422d9767
Nick Hosman <nickhosman@gmail.com>
2023-08-29 15:31:20 -0700

    dealt with small bug regarding avgRating on spot detail page

--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -19,19 +19,26 @@ export default function SpotDetails() {
     alert("Feature coming soon");
   };
 
+  const avgRating = Number.parseFloat(spot.avgStarRating).toFixed(2);
+
   return (
     <>
       <h1>{spot.name}</h1>
       <section>
         <i className="fa-solid fa-star fa-xs"></i>
-        {`${Number.parseFloat(spot.avgStarRating).toFixed(2)} `}‧{" "}
-        {spot.numReviews}
-        {parseInt(spot.numReviews) < 2 ? " review " : " reviews "}‧ {spot.city},{" "}
+        {isNaN(avgRating) ? "New" : avgRating}{" "}
+        {parseInt(spot.NumReviews) < 1
+          ? " ‧ "
+          : parseInt(spot.numReviews) === 1
+          ? " ‧ " + spot.numReviews + " review ‧ "
+          : " ‧ " + spot.numReviews + " reviews ‧ "}
         {spot.state}, {spot.country}
         <div className="img-wrapper">
-          {spot.SpotImages ? spot.SpotImages.map((image) => {
-            return <img alt="" key={image.id} src={image.url} />;
-          }) : null}
+          {spot.SpotImages
+            ? spot.SpotImages.map((image) => {
+                return <img alt="" key={image.id} src={image.url} />;
+              })
+            : null}
         </div>
         <div className="spot-info">
           <h2>
@@ -41,7 +48,9 @@ export default function SpotDetails() {
         </div>
         <div className="callout-info">
           <span className="spot-price">${spot.price} night</span>
-          <button className="reserve-button" onClick={handleClick}>Reserve</button>
+          <button className="reserve-button" onClick={handleClick}>
+            Reserve
+          </button>
         </div>
       </section>
     </>
